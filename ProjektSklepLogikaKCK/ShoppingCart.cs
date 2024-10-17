3f870c8b7780df3b7cb21278ad58d80ee9bdcef7
SylwekD <rutki2268@gmail.com>
2024-10-17 21:23:26 +0200

    VAT

--- ProjektSklepLogikaKCK/ShoppingCart.cs
+++ ProjektSklepLogikaKCK/ShoppingCart.cs
@@ -10,17 +10,20 @@ namespace ProjektSklepLogikaKCK
     {
         private List<Product> productsInCart = new List<Product>();
         private float fullprice;
+        private float fullpriceNetto;
 
         public void AddItem(Product product)
         {
             productsInCart.Add(product);
             fullprice += product.price;
+            fullpriceNetto += product.price *= ((float)(100 - product.category.vat) / 100);
         }
 
-        public void DeleteItem(Product product) 
+        public void DeleteItem(Product product)
         {
             productsInCart.Remove(product);
             fullprice -= product.price;
+            fullpriceNetto -= product.price *= ((float)(100 - product.category.vat) / 100);
         }
 
         public List<Product> GetProducts()
@@ -28,7 +31,9 @@ namespace ProjektSklepLogikaKCK
             return productsInCart;
         }
 
-        public float GetFullprice() {  return fullprice; }
+        public float GetFullprice() { return fullprice; }
+
+        public float GetFullpriceNetto() { return fullpriceNetto; }
 
 
 
@@ -36,7 +41,7 @@ namespace ProjektSklepLogikaKCK
         {
             foreach (Product product in productsInCart)
             {
-                productsInCart.Remove(product); fullprice -= product.price;
+                productsInCart.Remove(product); fullprice -= product.price; fullpriceNetto -= product.price *= ((float)(100 - product.category.vat) / 100);
             }
         }
 
